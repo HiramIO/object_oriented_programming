8ae34a79bf60f6e69e0fb3dab1c650f7e9ee203f
Harrison Taylor <h.taylor@project89.org>
2015-04-02 15:31:39 -0400

    Oh gods - Mars rover

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,47 +1,61 @@
 class Rover
-    def initialize(x, y, dir)
-        :x = x
-        :y = y
-        :dir = dir
+    def initialize(init_x, init_y, init_dir)
+        @x = init_x
+        @y = init_y
+        @dir = init_dir
     end
-    def read_instruction(turn, move)
-        puts "Turn L, R or No"
-            if turn == L
-                turn()
-    end
-    def turn(t)
-        # Alternatively, it could be an array? [N, E, S, W]? Good to explore later.
-        if t = L
-            if dir == N
-                dir = W
-            elsif dir == W
-                dir = S
-            elsif dir == S
-                dir = E
-            elsif dir == E
-                dir = N
+    def read_instruction(user_input)
+        commands = user_input.split
+            commands.each do |x|
+                if x == "L"
+                    turn_left
+                elsif x == "R"
+                    turn_right
+                elsif x == "M"
+                    move
+                end
             end
-        elsif t = R
-            if dir == N
-                dir = E
-            elsif dir == E
-                dir = S
-            elsif dir == S
-                dir = W
-            elsif dir == W
-                dir = N
+    end
+    def turn_left
+            if @dir == "N"
+                @dir = "W"
+            elsif @dir == "W"
+                @dir = "S"
+            elsif @dir == "S"
+                @dir = "E"
+            elsif @dir == "E"
+                @dir = "N"
             end
         end
-    end
+    def turn_right
+            if @dir == "N"
+                @dir = "E"
+            elsif @dir == "E"
+                @dir = "S"
+            elsif @dir == "S"
+                @dir = "W"
+            elsif @dir == "W"
+                @dir = "N"
+            end
+        end
     def move
-        if dir == N
-            x += 1
-        elsif dir == E
-            y += 1
-        elsif dir == S
-            x -= 1
-        elsif dir == W
-            y -= 1
+        if @dir == "N"
+            @y += 1
+        elsif @dir == "E"
+            @x += 1
+        elsif @dir == "S"
+            @y -= 1
+        elsif @dir == "W"
+            @x -= 1
         end
     end
+    def to_s
+    "I am at #{@x}, #{@y} facing #{@dir}."
     end
+    end
+
+    my_rover = Rover.new(0, 0, "N")
+    puts my_rover
+    my_rover.read_instruction("LMMRMMRMMMM")
+    puts my_rover
+
